032a51d37e6c6fc4f194edae87fb811dbba2fd93
ksu_kotovaa <ksu_kotovaa@mail.ru>
2025-01-29 18:55:23 +0300

    feat(database): add function for comparing even and odd values in MyTable

--- Lab1.sql
+++ Lab1.sql
@@ -13,5 +13,21 @@ BEGIN
         END LOOP;
 END;
 */
-SELECT * FROM MyTable;
-
+--SELECT * FROM MyTable;
+/*
+CREATE OR REPLACE FUNCTION compare_even_odd RETURN VARCHAR IS
+    v_even NUMBER;
+    v_odd NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_even FROM MYTABLE WHERE MOD(val, 2) = 0;
+    SELECT COUNT(*) INTO v_odd FROM MYTABLE WHERE MOD(val, 2) <> 0;
+    IF v_even > v_odd THEN
+        RETURN 'Even';
+    ELSIF v_even < v_odd THEN
+        RETURN 'Odd';
+    ELSE
+        RETURN 'Equal';
+    END IF;
+END;
+*/
+SELECT compare_even_odd() from dual;
