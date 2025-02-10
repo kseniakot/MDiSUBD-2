8cc5d2c6ae1af073932fb3c20f06c96c1473bd2e
ksu_kotovaa <ksu_kotovaa@mail.ru>
2025-02-10 13:16:59 +0300

    feat: implement autoincrement trigger for GROUPS and STUDENTS"

--- Lab2.sql
+++ Lab2.sql
@@ -14,3 +14,20 @@ CREATE TABLE students (
     CONSTRAINT student_id_pkey PRIMARY KEY (student_id),
     CONSTRAINT students_group_id_fkey FOREIGN KEY (gr_id) REFERENCES groups (group_id)
 );
+
+CREATE SEQUENCE GROUP_ID_SEQ START WITH 1;
+CREATE SEQUENCE STUDENT_ID_SEQ START WITH 1;
+
+CREATE OR REPLACE TRIGGER groups_id_autoincrement
+BEFORE INSERT ON groups
+    FOR EACH ROW
+BEGIN
+    :NEW.group_id := GROUP_ID_SEQ.NEXTVAL;
+END;
+
+CREATE OR REPLACE TRIGGER students_id_autoincrement
+BEFORE INSERT ON students
+    FOR EACH ROW
+BEGIN
+    :NEW.student_id := STUDENT_ID_SEQ.NEXTVAL;
+END;
