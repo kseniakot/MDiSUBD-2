5cd498d2dd66efdb0c9785fbc7be8454548f3eee
ksu_kotovaa <ksu_kotovaa@mail.ru>
2025-02-10 15:04:56 +0300

    fix: remove constraints from GROUPS and STUDENTS

--- Lab2.sql
+++ Lab2.sql
@@ -3,16 +3,13 @@ drop table students;
 CREATE TABLE groups (
     group_id NUMBER NOT NULL,
     group_name VARCHAR2(20) NOT NULL,
-    C_VAL NUMBER NOT NULL,
-    CONSTRAINT group_id_pkey PRIMARY KEY (group_id)
+    C_VAL NUMBER NOT NULL
 );
 
 CREATE TABLE students (
     student_id NUMBER NOT NULL,
     student_name VARCHAR2(20) NOT NULL,
-    gr_id NUMBER NOT NULL,
-    CONSTRAINT student_id_pkey PRIMARY KEY (student_id),
-    CONSTRAINT students_group_id_fkey FOREIGN KEY (gr_id) REFERENCES groups (group_id)
+    group_id NUMBER NOT NULL
 );
 
 CREATE SEQUENCE GROUP_ID_SEQ START WITH 1;
