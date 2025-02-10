5a8c17e2eb35d19ab6688a886b89ad4e4816f92e
ksu_kotovaa <ksu_kotovaa@mail.ru>
2025-02-10 13:16:23 +0300

    refactor: rename id in GROUPS to group_id

--- Lab2.sql
+++ Lab2.sql
@@ -1,9 +1,10 @@
-
+drop table groups;
+drop table students;
 CREATE TABLE groups (
-    id NUMBER NOT NULL,
+    group_id NUMBER NOT NULL,
     group_name VARCHAR2(20) NOT NULL,
     C_VAL NUMBER NOT NULL,
-    CONSTRAINT group_id_pkey PRIMARY KEY (id)
+    CONSTRAINT group_id_pkey PRIMARY KEY (group_id)
 );
 
 CREATE TABLE students (
@@ -11,7 +12,5 @@ CREATE TABLE students (
     student_name VARCHAR2(20) NOT NULL,
     gr_id NUMBER NOT NULL,
     CONSTRAINT student_id_pkey PRIMARY KEY (student_id),
-    CONSTRAINT students_group_id_fkey FOREIGN KEY (gr_id) REFERENCES groups (id)
+    CONSTRAINT students_group_id_fkey FOREIGN KEY (gr_id) REFERENCES groups (group_id)
 );
-
-
